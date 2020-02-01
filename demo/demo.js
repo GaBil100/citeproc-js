197504888ec1562885270ec43f21693d847476ac
Gareth Bilaney <gareth.bilaney@hhu.de>
2020-02-01 15:57:21 +0100

    Update demo.js

--- demo/demo.js
+++ demo/demo.js
@@ -96,7 +96,7 @@ function runRenderBib(idx) {
         // Bib
         var bibDiv = document.getElementById('bib-div');
         var bibResult = citeproc.makeBibliography();
-        bibDiv.innerHTML = bibResult[1].join('ASGF\n');
+        bibDiv.innerHTML = bibResult[1].join('\n');
     } else {
         setTimeout(function() {
             runOneStep(idx);
